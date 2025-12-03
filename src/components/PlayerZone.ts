753cfdfe040b00215eff7a2480d678a7eb4ac1f6
Darrenwu1006 <wuy529@newschool.edu>
2025-12-03 18:38:30 +0800

    fix: remove unused shuffle function

--- src/components/PlayerZone.ts
+++ src/components/PlayerZone.ts
@@ -584,14 +584,6 @@ export class PlayerZone {
     });
   }
 
-  private shuffle(array: any[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      ([array[i], array[j]] = array[j]), array[i];
-    }
-    return array;
-  }
-
   private attachHandEvents() {
     const handArea = this.element.querySelector(".hand-area");
     handArea?.addEventListener("click", (e) => {
